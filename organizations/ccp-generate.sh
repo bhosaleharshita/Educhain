2976a23bc1e35b9d16798b3f5ab092228cfc5c9d
Harshita <harshitabhosale5600@gmail.com>
2021-05-05 23:37:06 +0530

    connection profile bug fixed

--- organizations/ccp-generate.sh
+++ organizations/ccp-generate.sh
@@ -34,7 +34,7 @@ CAPEM=organizations/peerOrganizations/uni.example.com/ca/ca.uni.example.com-cert
 
 echo "$(json_ccp $ORG $P0PORT $CAPORT $PEERPEM $CAPEM)" > organizations/peerOrganizations/uni.example.com/connection-uni.json
 echo "$(yaml_ccp $ORG $P0PORT $CAPORT $PEERPEM $CAPEM)" > organizations/peerOrganizations/uni.example.com/connection-uni.yaml
-echo "$(yaml_ccp $ORG $P0PORT $CAPORT $PEERPEM $CAPEM)" > Application/organisation/uni/gateway/connection-uni.yaml
+echo "$(yaml_ccp $ORG $P0PORT $CAPORT $PEERPEM $CAPEM)" > Application/organization/uni/gateway/connection-uni.yaml
 
 ORG=mhrd
 P0PORT=9051
@@ -44,7 +44,7 @@ CAPEM=organizations/peerOrganizations/mhrd.example.com/ca/ca.mhrd.example.com-ce
 
 echo "$(json_ccp $ORG $P0PORT $CAPORT $PEERPEM $CAPEM)" > organizations/peerOrganizations/mhrd.example.com/connection-mhrd.json
 echo "$(yaml_ccp $ORG $P0PORT $CAPORT $PEERPEM $CAPEM)" > organizations/peerOrganizations/mhrd.example.com/connection-mhrd.yaml
-echo "$(yaml_ccp $ORG $P0PORT $CAPORT $PEERPEM $CAPEM)" > ../Application/organisation/mhrd/gateway/connection-mhrd.yaml
+echo "$(yaml_ccp $ORG $P0PORT $CAPORT $PEERPEM $CAPEM)" > Application/organization/mhrd/gateway/connection-mhrd.yaml
 
 
 ORG=thirdparty
@@ -55,5 +55,5 @@ CAPEM=organizations/peerOrganizations/thirdparty.example.com/ca/ca.thirdparty.ex
 
 echo "$(json_ccp $ORG $P0PORT $CAPORT $PEERPEM $CAPEM)" > organizations/peerOrganizations/thirdparty.example.com/connection-thirdparty.json
 echo "$(yaml_ccp $ORG $P0PORT $CAPORT $PEERPEM $CAPEM)" > organizations/peerOrganizations/thirdparty.example.com/connection-thirdparty.yaml
-echo "$(yaml_ccp $ORG $P0PORT $CAPORT $PEERPEM $CAPEM)" > ../Application/organisation/thirdparty/gateway/connection-thirdparty.yaml
+echo "$(yaml_ccp $ORG $P0PORT $CAPORT $PEERPEM $CAPEM)" > Application/organization/thirdparty/gateway/connection-thirdparty.yaml
 
